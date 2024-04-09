1fe29e93fd6e1380e30acff7517a580078a8f88c
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2024-04-09 18:39:16 +0530

    Update canvas.jsx

--- Client/src/Pages/canvas.jsx
+++ Client/src/Pages/canvas.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import { ChromePicker } from 'react-color';
 
-const socket = io.connect('https://pix-backend.netlify.app');
+const socket = io.connect('https://pixel-io.onrender.com');
 
 export default function Canvas() {
   const [color, setColor] = useState('black');
